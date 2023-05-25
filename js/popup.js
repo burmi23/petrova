ec4d46e08d27fd238520fd936c61ec126538fcf0
Nikita Burmistrov <dev.burmi@gmail.com>
2023-05-25 21:43:13 +0300

    корректировки после встречи

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,12 @@
 let popupBtn = document.querySelector(".agree__btn-yes");
+let main = document.querySelector("main");
+let windowAlert = document.querySelector(".agree__on");
+let card = document.querySelector(".book__bottom");
+
+
+popupBtn.addEventListener("click", function(){
+  document.body.classList.remove("stop-scroll");
+  main.classList.remove("blur-on-off");
+  windowAlert.classList.add("agree__off");
+  card.classList.remove("block-off");
+})
